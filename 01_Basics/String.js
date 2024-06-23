8cc078cbcd242251818983e277e7d4e4e170bfb7
Harshita Barfa <harshita.barfa3@outlook.com>
2024-06-24 00:07:06 +0530

    update string.js

---
+++ 01_Basics/String.js
@@ -0,0 +1,43 @@
+// String : String are used to store and manipulate text.
+
+const name = "Harshita"
+const repoCount = 50
+
+// String ko declare karne ka ek or tarika.
+const otherName = new String("Harshiii")
+
+// Accessing element
+// console.log(name[0])
+
+// Concatenate method
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+// Length method
+// console.log(name.length)
+
+// toUpperCase() method
+// console.log(name.toUpperCase())
+
+// toLowerCase() method
+// console.log(name.toLowerCase())
+
+// Slice() method
+// console.log(name.slice(2,5)) // Index 2 se 4 tak, 5 not include
+// console.log(name.slice(3)) // Index 3 se last index tak
+
+// replace() method
+// console.log(name.replace("Harshita", "Harshii"))
+
+// trim() method
+// let a = "  Apple  "
+// console.log(a.trim()) // Removes whitespaces
+
+// indexOf() method
+//console.log(name.indexOf('a')) // Konsa word kis index pe hai
+
+// charAt() method
+// console.log(name.charAt(2)) // Konse index pe konsa word hai
+
+// substring() method
+// console.log(name.substring(0,4)) // Index 0 se 3 tak, 4 not include
+
